ab8951469b37ff3b349e6f217008996dfce18da5
andrearafanelli <andrea.rafanelli96@gmail.com>
2024-02-21 16:11:15 +0100

    feat: minor

--- network.py
+++ network.py
@@ -16,30 +16,160 @@ import torch.nn as nn
 from torchvision import models
 
 
-class Model(nn.Module):
-    def __init__(self, num_classes=7):
-        super(Model, self).__init__()
-        model = models.mobilenet_v2(pretrained=True)
-        model.features[0][0] = nn.Conv2d(1, 32, kernel_size=3, stride=2, padding=1, bias=False)
-        self.features = model.features
-        self.global_avg_pooling = nn.AdaptiveAvgPool2d(1)
-        self.dense1 = nn.Linear(1280, 512)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dense2 = nn.Linear(512, 256)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.dropout2 = nn.Dropout(0.25)
-        self.dense3 = nn.Linear(256, num_classes)
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=16, dilation=1, norm_layer=None):
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 16:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        x = self.features(x)
-        x = self.global_avg_pooling(x)
-        x = x.view(x.size(0), -1)
-        x = self.dense1(x)
-        x = self.relu1(x)
-        x = self.dropout1(x)
-        x = self.dense2(x)
-        x = self.relu2(x)
-        x = self.dropout2(x)
-        x = self.dense3(x)
-        return x
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(SEBlock, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction_ratio),
+            nn.ReLU(inplace=True),
+            nn.Linear(in_channels // reduction_ratio, in_channels),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+class ResNetSE(nn.Module):
+    def __init__(self, block, layers, num_classes=7, dropout_prob=0.5):
+        super(ResNetSE, self).__init__()
+        self.nb_head = 512
+        self._norm_layer = nn.BatchNorm2d
+        self.inplanes = 64
+        self.dilation = 1
+        self.groups = 1
+        self.base_width = 16
+
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = self._norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        # SE blocks
+        self.se1 = SEBlock(64)
+        self.se2 = SEBlock(128)
+        self.se3 = SEBlock(256)
+        self.se4 = SEBlock(512)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        self.dropout = nn.Dropout(dropout_prob)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        previous_dilation = self.dilation
+
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                self._norm_layer(planes * block.expansion),
+            )
+
+        layers = [block(self.inplanes, planes, stride, downsample, self.groups,
+                        self.base_width, previous_dilation, self._norm_layer)]
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=self._norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.se1(x)
+        x = self.layer2(x)
+        x = self.se2(x)
+        x = self.layer3(x)
+        x = self.se3(x)
+        x = self.layer4(x)
+        x = self.se4(x)
+
+        x = self.avgpool(x)
+        f = torch.flatten(x, 1)
+        f = self.dropout(f)
+        out = self.fc(f)
+
+        return out
+
+
+def model(dropout_prob=0.5, **kwargs):
+    resnet = ResNetSE(BasicBlock, [2, 2, 2, 2], dropout_prob=dropout_prob, **kwargs)
+    resnet.fc = nn.Linear(512, 7)
+    return resnet
