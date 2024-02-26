0399dc492a2f1352435d4a3b30937cb96f053db8
andrearafanelli <andrea.rafanelli96@gmail.com>
2024-02-26 18:47:09 +0100

    feat:plot fx

--- metrics.py
+++ metrics.py
@@ -11,9 +11,11 @@ See the License for the specific language governing permissions and limitations 
 
 __author__ = 'Andrea Rafanelli'
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 import torch
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def calculate_accuracy(predictions, targets):
@@ -59,6 +61,7 @@ class Score:
         metrics['Precision'] = precision_score(y_true, y_pred, average='macro', zero_division=1.0)
         f1_scores = f1_score(y_true, y_pred, average=None, labels=labels)
         metrics['F1'] = f1_scores.sum() / labels.shape[0]
+        metrics['ConfusionMatrix'] = confusion_matrix(y_true, y_pred)
 
         self.print_metrics(metrics, phase)
         return metrics
@@ -66,7 +69,28 @@ class Score:
     @staticmethod
     def print_metrics(metrics, phase):
         outputs = []
-        for k in metrics.keys():
-            outputs.append("{}: {:4f}".format(k, metrics[k]))
-        print("{}: {}".format(phase, ", ".join(outputs)))
+        try:
+            for k in metrics.keys():
+                if k != 'Confusion Matrix':
+                    outputs.append("{}: {:4f}".format(k, metrics[k]))
 
+            print("{}: {}".format(phase, ", ".join(outputs)))
+        except:
+            pass
+
+
+def plot(metrics):
+    class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+    plt.figure(figsize=(12, 5))
+    plt.rcParams["font.size"] = 12
+    plt.rcParams["font.weight"] = "bold"
+    sns.heatmap(metrics['ConfusionMatrix'], annot=True, fmt='d', cmap='Blues', xticklabels=class_names,
+                yticklabels=class_names)
+    plt.xlabel('\nPredicted', size=15, color='orangered')
+    plt.ylabel('Ground Truth\n', size=15, color='dodgerblue')
+    plt.title('Confusion Matrix', size=20, color='teal', fontweight="bold")
+
+    plt.text(0.3, -0.5, f"Test Accuracy: {100 * metrics['Accuracy']:.2f} %", fontsize=12, color='crimson',
+             bbox=dict(facecolor='white', edgecolor='crimson', boxstyle='round,pad=0.4'))
+
+    plt.show()
